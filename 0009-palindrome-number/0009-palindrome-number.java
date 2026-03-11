f0ad18e7248e7e2430884da634a2a349adbd2b60
Abdelrahman Mohamed Hammad Hassan Wally <Abdulrahmanwally77@gmail.com>
2026-03-11 14:13:35 +0200

    Time: 17 ms (5.82%), Space: 46.7 MB (5.91%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -4,17 +4,13 @@ class Solution {
         if(x < 0){return  false;}
         if(x == 0){return true;}
         if(x >= Integer.MIN_VALUE && x <= Integer.MAX_VALUE ){
-            int i=0;
             String newNumber = "";
             int CopyNumber =x;
            while(CopyNumber > 0){
 
              newNumber =newNumber +CopyNumber % 10;
                CopyNumber =CopyNumber /10;
-
-               i++;
            }
-            System.out.println(newNumber);
            if(newNumber.equalsIgnoreCase(String.valueOf(x))){
                return true;
            }
