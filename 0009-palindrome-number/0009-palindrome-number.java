410abcb3514affaf43d2bf3daab84b6091b6ae25
Abdelrahman Mohamed Hammad Hassan Wally <Abdulrahmanwally77@gmail.com>
2026-03-11 14:13:02 +0200

    Time: 41 ms (5.82%), Space: 46.9 MB (5.91%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        var numberOfDigit = numberOfDigit(x);
+    
         if(x < 0){return  false;}
         if(x == 0){return true;}
         if(x >= Integer.MIN_VALUE && x <= Integer.MAX_VALUE ){
             int i=0;
             String newNumber = "";
             int CopyNumber =x;
-           while(i < numberOfDigit){
+           while(CopyNumber > 0){
 
              newNumber =newNumber +CopyNumber % 10;
                CopyNumber =CopyNumber /10;
@@ -22,16 +22,5 @@ class Solution {
         }
         return false;
     }
-    
-    public   int numberOfDigit(int number){
 
-        int numberOfdigit =0;
-        number = Math.abs(number);
-        if(number ==0){return  1;}
-        while (number > 0){
-            number =number /10;
-            numberOfdigit++;
-        }
-        return numberOfdigit;
-    }
 }
